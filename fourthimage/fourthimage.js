97f5b7de2fe98ec182df29e19fe04ae9f8a14d0f
AmeyPank <pankantiameya@gmail.com>
2023-02-03 15:27:18 +0530

    solution for f2 module test

--- fourthimage/fourthimage.js
+++
@@ -1,4 +0,0 @@
-function rand(){
-    let x = parseInt(Math.random()*1000000000000);
-    document.getElementById('result').innerHTML = x;
-}
--- thirdimage/thirdimage.js
+++
@@ -1,6 +0,0 @@
-function roll(){
-   
-   var rand = Math.floor(Math.random()*6)+1;
-   document.getElementById('result').innerHTML = 
-   "<img src = 'image/"+rand+".png'/>"
-}
